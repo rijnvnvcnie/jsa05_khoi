6f04ce32ec9f00bc88248e2ccd13f3ae2b138981
rijnvnvcnie <leminhkhoi100808@gmail.com>
2023-08-13 12:04:15 +0700

    buoi 2

---
+++ script.js
@@ -0,0 +1,10 @@
+let myNumber = [0 ,1 ,2];
+let w = ["kjh" ,"fds" ,"qwe"];
+//console.log(w[1]);
+//console.log(myNumber);
+//console.log(w);
+let i = 0;
+while (i < 3) {
+    console.log(i + ": " + w[i]);
+    i++;
+}
