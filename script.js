da7d60150df4df3119aef72ee94d20ad145f5330
rijnvnvcnie <leminhkhoi100808@gmail.com>
2023-08-20 11:27:39 +0700

    buoi 2

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-let myNumber = [0 ,1 ,2];
-let w = ["kjh" ,"fds" ,"qwe"];
-//console.log(w[1]);
-//console.log(myNumber);
-//console.log(w);
-let i = 0;
-while (i < 3) {
-    console.log(i + ": " + w[i]);
-    i++;
+//let myNumber = [4, 5, 6, 7, 8, 9, 1];
+  ///  console.log(myNumber)
+   /// for( let i =0; i<myNumber.length; i++){
+      ///  console.log(myNumber[i])
+    ///}
+    ///myNumber.splice(2, 1, 7, 8);
+   /// console.log(myNumber)
+  /// console.log(myNumber.length)
+  ///myNumber.push(7)
+  ///console.log(myNumber)
+  //myNumber.pop()
+  //console.log(myNumber)
+  //myNumber.unshift(0)
+  //console.log(myNumber)
+  //let heo = {
+  //  name: 'Nguyễn Minh Hiến',
+    //age: 14
+ // }
+  //heo.name = 'sdsfdgfh'
+//  console.log(dog);
+//heo.action = 'ngu'
+//console.log(heo);
+//let đẹp trai = new Object ({
+ //   name: Khoi
+   // age: 14
+   // enemy : {
+   //     name: "Nguyễn Minh Hiến"
+   //     age:14
+   // }
+//})
+//let dog1 =["A", 4]
+//let dog2 =["C", 5]
+//let doge = [dog1, dog2]
+//console.log(doge)
+let gtc = [
+    ["x", 2],
+    ["dh", 3]
+]
+for(let i =0; i<gtc.length; i++){
+    console.log("gio trai cay: " + gtc[i][0] + gtc[i][1])
 }
