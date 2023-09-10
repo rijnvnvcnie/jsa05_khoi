2d8bb2e92a903a9241ae8467813ae05184b56e0b
rijnvnvcnie <leminhkhoi100808@gmail.com>
2023-09-10 12:07:02 +0700

    buoi5

---
+++ new.js
@@ -0,0 +1,16 @@
+var button = document.getElementById('login');
+button.addEventListener("click", function(){
+
+}
+)
+document.getElementById("loginForm").addEventListener("submit", function(event){
+
+var username = document.getElementById(username).value;
+var password = document.getElementById(password).value;
+if(username == "fgh" && password == "123"){
+    localStorage.getItem(key, username)
+}else{
+    alert("dangnhapthatbai")
+}
+}
+)
